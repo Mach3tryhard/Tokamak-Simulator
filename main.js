ecf2bf134345287fc1e45b6d671d9ea8743ab63e
Mach3tryhard <matei.sirghe@gmail.com>
2023-09-13 20:15:13 +0300

    start

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
