8755fd4c72cad736b39f70a043e31e62d6aad6f6
Mach3tryhard <matei.sirghe@gmail.com>
2023-09-14 12:25:37 +0300

    0.0.4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'three';	
 
 const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,24 +8,41 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0xF8C8DC } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.set( 0, 0, 100 );
 camera.lookAt( 0, 0, 0 );
 camera.position.z = 5;
+camera.position.y = 1;
 
 function animate() {
-	
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	cube.rotation.z += 0.01;
+	cube.position.x+=0.0001;
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
+	//cube.rotation.z += 0.01;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
-animate();
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        cube.position.y += ySpeed;
+    } else if (keyCode == 83) {
+        cube.position.y -= ySpeed;
+    } else if (keyCode == 65) {
+        cube.position.x -= xSpeed;
+    } else if (keyCode == 68) {
+        cube.position.x += xSpeed;
+    } else if (keyCode == 32) {
+        cube.position.set(0, 0, 0);
+    }
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+};
 
-/*addEventListener("resize", (event) => {
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-});*/
+var xSpeed = 0.0001;
+var ySpeed = 0.0001;
+animate();
