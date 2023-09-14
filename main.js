f78ee44700ea9839d3d2ab8f8c63060fc644973b
Mach3tryhard <matei.sirghe@gmail.com>
2023-09-14 09:54:45 +0300

    0.0.3

--- main.js
+++ main.js
@@ -12,9 +12,12 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 camera.position.z = 5;
 
 function animate() {
+	
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	cube.rotation.z += 0.01;
