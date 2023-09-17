0f6fd90b89ecd8168b23593dee5bd7554ed6119d
Mach3tryhard <matei.sirghe@gmail.com>
2023-09-17 16:06:20 +0300

    0.1.1

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 /// Scene preparation
 
-///STATS
+///STATS 
 (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
 
 /// THREE
